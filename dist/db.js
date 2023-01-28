3d78e520511df2196b4839f520fce45be6f0bd50
tpalinski <tpalinski@icloud.com>
2023-01-28 22:58:15 +0100

    put database password in env

--- dist/db.js
+++ dist/db.js
@@ -1,16 +1,26 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectToDatabase = void 0;
 const mongodb_1 = require("mongodb");
-const uri = "mongodb+srv://admin:Klopklop2@cluster0.ipgs6c8.mongodb.net/?retryWrites=true&w=majority";
+require('dotenv').config();
+const password = process.env.MONGO_PASSWORD;
+const uri = "mongodb+srv://admin:" + password + "@cluster0.ipgs6c8.mongodb.net/?retryWrites=true&w=majority";
 //@ts-ignore
 const client = new mongodb_1.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: mongodb_1.ServerApiVersion.v1 });
 function connectToDatabase() {
-    client.connect(err => {
-        const collection = client.db("test").collection("devices");
-        // perform actions on the collection object
-        console.log("Connected to the database");
-        client.close();
+    return __awaiter(this, void 0, void 0, function* () {
+        client.connect().then((res) => {
+            console.log("Connected to database ");
+        });
     });
 }
 exports.connectToDatabase = connectToDatabase;
